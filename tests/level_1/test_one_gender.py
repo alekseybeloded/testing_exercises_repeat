794e248e26351fff6b2ce8a5e06869797b06c6d3
alekseybeloded <belod-aleksej@yandex.ru>
2023-11-21 20:37:26 +0300

    test: Add tests for 100% coverage

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,5 +1,17 @@
 from functions.level_1.one_gender import genderalize
+import pytest
 
 
-def test_genderalize():
-    pass
+def test__genderalize__return_verb_male():
+    assert genderalize(verb_male='male', verb_female='female', gender='male') == 'male'
+
+
+@pytest.mark.parametrize(
+    'verb_male, verb_female, gender, expected_result',
+    [
+        pytest.param('male', 'female', '', 'female', id='gender is empty'),
+        pytest.param('male', 'female', 'abc', 'female', id='gender is not equal "male"'),
+    ]
+)
+def test__genderalize__return_verb_female(verb_male, verb_female, gender, expected_result):
+    assert genderalize(verb_male, verb_female, gender) == expected_result
